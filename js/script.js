7d30f19b9e262f786a8f980362c79e8879ec107f
sameh hussien <riotteam61@gmail.com>
2023-12-02 21:18:08 +0200

    first commit

---
+++ js/script.js
@@ -0,0 +1,8 @@
+var typed = new Typed(".typed", {
+  strings: ["Larry Daniels", "Developer", "Designer"],
+  loop: true,
+  typeSpeed: 30,
+  backSpeed: 30,
+  backDelay: 600,
+  smartBackspace: false,
+});
